8dc6789e7a5c95543ae25ae57ec215529c075b00
Axel Gembe <derago@gmail.com>
2022-01-07 07:43:27 +0000

    httpserver: Address review comments from !1429
    Summary
    ---
    
    * Replaces some `push_back` with `emplace_back` calls.
    * Adds an else between two mutually exlusive  if statements.
    * Changes an iterator for loop into a range-based for loop.
    
    Test plan
    ---
    
    * `ninja all check-all`

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -334,12 +334,11 @@ static bool HTTPBindAddresses(struct evhttp *http) {
     // Determine what addresses to bind to
     if (!(gArgs.IsArgSet("-rpcallowip") && gArgs.IsArgSet("-rpcbind"))) {
         // Default to loopback if not allowing external IPs
-        endpoints.push_back(std::make_pair("::1", http_port));
-        endpoints.push_back(std::make_pair("127.0.0.1", http_port));
+        endpoints.emplace_back("::1", http_port);
+        endpoints.emplace_back("127.0.0.1", http_port);
         if (gArgs.IsArgSet("-rpcallowip")) {
             LogPrintf("WARNING: option -rpcallowip was specified without -rpcbind; this doesn't usually make sense\n");
-        }
-        if (gArgs.IsArgSet("-rpcbind")) {
+        } else if (gArgs.IsArgSet("-rpcbind")) {
             LogPrintf("WARNING: option -rpcbind was ignored because "
                       "-rpcallowip was not specified, refusing to allow "
                       "everyone to connect\n");
@@ -350,28 +349,24 @@ static bool HTTPBindAddresses(struct evhttp *http) {
             int port = http_port;
             std::string host;
             SplitHostPort(strRPCBind, port, host);
-            endpoints.push_back(std::make_pair(host, port));
+            endpoints.emplace_back(std::move(host), port);
         }
     }
 
     // Bind addresses
-    for (std::vector<std::pair<std::string, uint16_t>>::iterator i =
-             endpoints.begin();
-         i != endpoints.end(); ++i) {
-        LogPrint(BCLog::HTTP, "Binding RPC on address %s port %i\n", i->first,
-                 i->second);
-        evhttp_bound_socket *bind_handle = evhttp_bind_socket_with_handle(
-            http, i->first.empty() ? nullptr : i->first.c_str(), i->second);
+    for (const auto &[host, port] : endpoints) {
+        LogPrint(BCLog::HTTP, "Binding RPC on address %s port %i\n", host, port);
+        evhttp_bound_socket *bind_handle =
+            evhttp_bind_socket_with_handle(http, host.empty() ? nullptr : host.c_str(), port);
         if (bind_handle) {
             CNetAddr addr;
-            if (i->first.empty() || (LookupHost(i->first, addr, false) && addr.IsBindAny())) {
+            if (host.empty() || (LookupHost(host, addr, false) && addr.IsBindAny())) {
                 LogPrintf("WARNING: the RPC server is not safe to expose to untrusted networks such as the public "
                           "internet\n");
             }
             boundSockets.push_back(bind_handle);
         } else {
-            LogPrintf("Binding RPC on address %s port %i failed.\n", i->first,
-                      i->second);
+            LogPrintf("Binding RPC on address %s port %i failed.\n", host, port);
         }
     }
     return !boundSockets.empty();
