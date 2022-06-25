9dee9c365597d52538176f2039670dcbd95d59aa
SlowRiot <slowriot@voxelstorm.com>
2022-06-25 08:07:58 +0000

    Remove deprecated setexcessiveblock RPC call
    Resolves #426.
    
    The setexcessiveblock RPC call was deprecated in v24.1.0.
    
    Changes
    -------
    
    * Remove setexessiveblock RPC call.
    * Remove setexessiveblock unit tests.
    * Remove setexessiveblock functional tests.
    * Where setexcessiveblock RPC calls are used in other functional tests,
      the functionality is replaced with node restarts using an appropriate
      -excessiveblock argument.
    
    Test plan
    ---------
    
    * `ninja check-all`
    
    Two additional tests which are not part of the default functional suite,
    but have been touched by this change - these take a while to run:
    
    * `./test/functional/test_runner.py abc-p2p-compactblocks.py`
    * `./test/functional/test_runner.py abc-p2p-fullblocktest.py`

--- src/test/excessiveblock_tests.cpp
+++ src/test/excessiveblock_tests.cpp
@@ -9,63 +9,12 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <limits>
-#include <string>
-
 extern UniValue CallRPC(const std::string &strMethod, bool multithreaded = false);
 
 BOOST_FIXTURE_TEST_SUITE(excessiveblock_tests, TestingSetup)
 
 BOOST_AUTO_TEST_CASE(excessiveblock_rpc) {
     BOOST_CHECK_NO_THROW(CallRPC("getexcessiveblock"));
-
-    BOOST_CHECK_THROW(CallRPC("setexcessiveblock"), std::runtime_error);
-    BOOST_CHECK_THROW(CallRPC("setexcessiveblock not_uint"),
-                      std::runtime_error);
-    BOOST_CHECK_THROW(CallRPC("setexcessiveblock 1000000 not_uint"),
-                      std::runtime_error);
-    BOOST_CHECK_THROW(CallRPC("setexcessiveblock 1000000 1"),
-                      std::runtime_error);
-    BOOST_CHECK_THROW(CallRPC("setexcessiveblock -1"), std::runtime_error);
-
-    BOOST_CHECK_THROW(CallRPC("setexcessiveblock 0"), std::runtime_error);
-    BOOST_CHECK_THROW(CallRPC("setexcessiveblock 1"), std::runtime_error);
-    BOOST_CHECK_THROW(CallRPC("setexcessiveblock 1000"), std::runtime_error);
-    BOOST_CHECK_THROW(CallRPC(std::string("setexcessiveblock ") +
-                              std::to_string(ONE_MEGABYTE - 1)),
-                      std::runtime_error);
-    BOOST_CHECK_THROW(CallRPC(std::string("setexcessiveblock ") +
-                              std::to_string(ONE_MEGABYTE)),
-                      std::runtime_error);
-
-    BOOST_CHECK_NO_THROW(CallRPC(std::string("setexcessiveblock ") +
-                                 std::to_string(ONE_MEGABYTE + 1)));
-    BOOST_CHECK_NO_THROW(CallRPC(std::string("setexcessiveblock ") +
-                                 std::to_string(ONE_MEGABYTE + 10)));
-
-    // Default can be higher than 1MB in future - test it too
-    BOOST_CHECK_NO_THROW(CallRPC(std::string("setexcessiveblock ") +
-                                 std::to_string(DEFAULT_EXCESSIVE_BLOCK_SIZE)));
-    BOOST_CHECK_NO_THROW(CallRPC(std::string("setexcessiveblock ") +
-                                 std::to_string(DEFAULT_EXCESSIVE_BLOCK_SIZE * 8)));
-
-
-    // Setting excessive block size outside MAX_EXCESSIVE_BLOCK_SIZE is not allowed
-    BOOST_CHECK_THROW(CallRPC(std::string("setexcessiveblock ") + std::to_string(MAX_EXCESSIVE_BLOCK_SIZE + 1)),
-                      std::runtime_error);
-    // However, setting it at the limit should be ok
-    BOOST_CHECK_NO_THROW(CallRPC(std::string("setexcessiveblock ") + std::to_string(MAX_EXCESSIVE_BLOCK_SIZE)));
-
-    BOOST_CHECK_THROW(
-        CallRPC(
-            std::string("setexcessiveblock ") +
-            std::to_string(uint64_t(std::numeric_limits<int64_t>::max()) + 1)),
-        std::runtime_error);
-
-    BOOST_CHECK_THROW(
-        CallRPC(std::string("setexcessiveblock ") +
-                std::to_string(std::numeric_limits<uint64_t>::max())),
-        std::runtime_error);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
