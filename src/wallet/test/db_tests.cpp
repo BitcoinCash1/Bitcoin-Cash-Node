d7d33557d759593b7a66cc48a05ac10b93c6b183
mainnet-pat <mainnet_pat@protonmail.com>
2022-11-10 20:04:49 +0000

    [build] Fix build fail for boost 1.80.0
    See investigations in #436 .
    Use of the Boost-internal (non-API!) BOOST_FILESYSTEM_C_STR has been replaced by string().
    
    Test plan:
    
    (on various platforms, both with Boost 1.80+ and existing platforms with
    older Boost versions):
    
    - Run `ninja check-all`

--- src/wallet/test/db_tests.cpp
+++ src/wallet/test/db_tests.cpp
@@ -18,7 +18,7 @@ BOOST_AUTO_TEST_CASE(getwalletenv_file) {
     std::string test_name = "test_name.dat";
     fs::path datadir = SetDataDir("tempdir");
     fs::path file_path = datadir / test_name;
-    std::ofstream f(file_path.BOOST_FILESYSTEM_C_STR);
+    std::ofstream f(file_path.string());
     f.close();
 
     std::string filename;
