9a767b3c9db219c772d53292a65e2cd8f2d52211
Axel Gembe <derago@gmail.com>
2023-08-17 17:40:58 +0700

    [build] Remove unnecessary _Event_WINDOWS_LIBRARIES
    Summary
    ---
    
    This was removed in https://reviews.bitcoinabc.org/D5576 but not removed
    in the backport:
    
    https://gitlab.com/bitcoin-cash-node/bitcoin-cash-node/-/merge_requests/500
    
    This is unnecessary because now `find_component` sets the interface
    link libraries.
    
    Test plan
    ---
    
    * `ninja all check-all`

--- cmake/modules/FindEvent.cmake
+++ cmake/modules/FindEvent.cmake
@@ -50,12 +50,6 @@ if(Event_INCLUDE_DIR)
 		INTERFACE_LINK_LIBRARIES "$<$<PLATFORM_ID:Windows>:ws2_32;shell32;advapi32>"
 	)
 
-	set(_Event_WINDOWS_LIBRARIES "$<$<PLATFORM_ID:Windows>:ws2_32;shell32;advapi32>")
-	set_property(TARGET Event::event
-		PROPERTY INTERFACE_LINK_LIBRARIES ${_Event_WINDOWS_LIBRARIES}
-	)
-	list(APPEND Event_LIBRARIES ${_Event_WINDOWS_LIBRARIES})
-
 	pkg_check_modules(PC_Event_pthreads QUIET event_pthreads libevent_pthreads)
 	find_component(Event pthreads
 		NAMES event_pthreads
