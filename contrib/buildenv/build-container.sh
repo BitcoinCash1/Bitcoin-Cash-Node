b7b2e762105b70f7d739da730e70d4fc73378468
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2024-05-20 14:58:06 +0200

    ci: Bump docker image buster -> bookworm

--- contrib/buildenv/build-container.sh
+++ contrib/buildenv/build-container.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+export LC_ALL=C
 set -e
-docker build --no-cache -t bitcoincashnode/buildenv:debian-v2 .
-#docker push bitcoincashnode/buildenv:debian-v2
+docker build --no-cache -t bitcoincashnode/buildenv:debian-v3 .
+#docker push bitcoincashnode/buildenv:debian-v3
