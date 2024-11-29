1d5a02e8c8de06fb57243a4260668c3ba8b890b6
Calin Culianu <calin.culianu@gmail.com>
2024-11-29 18:27:58 +0200

    Added partial backport of core header/cpp file "util/check.*"
    This updates parts of util/check.h to latest from Core, so we have it,
    since their improvements are decent. Also adds src/check.cpp.

--- src/util/check.h
+++ src/util/check.h
@@ -1,36 +1,44 @@
-// Copyright (c) 2019 The Bitcoin Core developers
+// Copyright (c) 2019-2022 The Bitcoin Core developers
 // Copyright (c) 2024 The Bitcoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #pragma once
 
-#include <clientversion.h>
-#include <tinyformat.h>
-
 #include <stdexcept>
+#include <string_view>
+#include <utility>
+
+std::string StrFormatInternalBug(std::string_view msg, std::string_view file, int line, std::string_view func);
 
 struct NonFatalCheckError : std::runtime_error {
-    using std::runtime_error::runtime_error;
+    NonFatalCheckError(std::string_view msg, std::string_view file, int line, std::string_view func);
 };
+
+/** Helper for CHECK_NONFATAL() */
+template <typename T>
+T&& inline_check_non_fatal(T&& val, const char* file, int line, const char* func, const char* assertion) {
+    if (!val) {
+        throw NonFatalCheckError{assertion, file, line, func};
+    }
+    return std::forward<T>(val);
+}
+
+// All macros may use __func__ inside a lambda, so put them under nolint.
+// NOLINTBEGIN(bugprone-lambda-function-name)
+
+#define STR_INTERNAL_BUG(msg) StrFormatInternalBug((msg), __FILE__, __LINE__, __func__)
+
 /**
- * Throw a NonFatalCheckError when the condition evaluates to false
+ * Identity function. Throw a NonFatalCheckError when the condition evaluates to false
  *
  * This should only be used
  * - where the condition is assumed to be true, not for error handling or validating user input
  * - where a failure to fulfill the condition is recoverable and does not abort the program
  *
- * For example in RPC code, where it is undersirable to crash the whole program, this can be generally used to replace
+ * For example in RPC code, where it is undesirable to crash the whole program, this can be generally used to replace
  * asserts or recoverable logic errors. A NonFatalCheckError in RPC code is caught and passed as a string to the RPC
  * caller, which can then report the issue to the developers.
  */
-#define CHECK_NONFATAL(condition)                                             \
-    do {                                                                      \
-        if (!(condition)) {                                                   \
-            throw NonFatalCheckError(                                         \
-                strprintf("Internal bug detected: %s\n%s:%d (%s)\n%s %s\n"    \
-                          "Please report this issue to the developers\n",     \
-                          (#condition), __FILE__, __LINE__, __func__,         \
-                          CLIENT_NAME, FormatFullVersion()));                 \
-        }                                                                     \
-    } while (false)
+#define CHECK_NONFATAL(condition) \
+    inline_check_non_fatal(condition, __FILE__, __LINE__, __func__, #condition)
